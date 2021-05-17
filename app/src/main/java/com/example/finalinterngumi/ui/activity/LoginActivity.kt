fe9daa01d8ed65ff88dbeefafa8dd3d818427557
Quốc Bảo <baokiinkk@gmail.com>
2021-05-17 11:25:53 +0700

    Revert "170521-stt02-loginByFacebook+loginByPhone"
    This reverts commit 18d8b6dc3adb221376aea63ad58f14690fa5bcb9.

--- app/src/main/java/com/example/finalinterngumi/ui/activity/LoginActivity.kt
+++ app/src/main/java/com/example/finalinterngumi/ui/activity/LoginActivity.kt
@@ -1,27 +1,27 @@
 package com.example.finalinterngumi.ui.activity
 
 import android.content.Intent
-
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import coil.load
 import com.example.finalinterngumi.R
 import com.example.projectgumi.sns.SNSLoginActivity
 import com.example.projectgumi.utils.SNSLoginType
 import com.example.projectgumi.utils.Utils
-import com.example.projectgumi.utils.Utils.SNS_LOGIN_TYPE
-import com.example.projectgumi.utils.Utils.SNS_REQUEST_CODE
-import com.example.projectgumi.utils.Utils.SNS_RESULT_CODE
-import com.example.projectgumi.utils.Utils.USER
+import com.facebook.CallbackManager
+import com.facebook.login.LoginManager
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_login.*
 
-
 class LoginActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
-
+    private lateinit var googleSignInClient: GoogleSignInClient
+    private lateinit var callbackManager: CallbackManager
 
     override fun onStart() {
         super.onStart()
@@ -37,19 +37,11 @@ class LoginActivity : AppCompatActivity() {
             }
         }
     }
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if(requestCode == SNS_REQUEST_CODE && resultCode == SNS_RESULT_CODE){
-            val user = data?.extras?.get(Utils.SNS_RESULT_DATA) as FirebaseUser?
-            updateUI(user)
-        }
-    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         init()
-
-
     }
 
     private fun init() {
@@ -65,18 +57,19 @@ class LoginActivity : AppCompatActivity() {
         button_loginPhone.setOnClickListener {
             singInPhone()
         }
+        button_logout.setOnClickListener {
+            signOut()
+        }
 
     }
     private fun singInPhone() {
-           val intent = Intent(this, SNSLoginActivity::class.java)
-        intent.putExtra(SNS_LOGIN_TYPE, SNSLoginType.PhoneNumber)
-        startActivityForResult(intent, SNS_REQUEST_CODE)
+//        val intent = Intent(this, PhoneLoginActivity::class.java)
+//        startActivity(intent)
     }
 
     private fun signInFb() {
-        val intent = Intent(this, SNSLoginActivity::class.java)
-        intent.putExtra(Utils.SNS_LOGIN_TYPE, SNSLoginType.Facebook)
-        startActivityForResult(intent, Utils.SNS_REQUEST_CODE)
+        LoginManager.getInstance()
+            .logInWithReadPermissions(this, arrayListOf("public_profile", "user_friends"))
     }
 
     private fun signInGg() {
@@ -85,11 +78,20 @@ class LoginActivity : AppCompatActivity() {
         startActivityForResult(intent, Utils.SNS_REQUEST_CODE)
     }
 
-//
+    private fun signOut() {
+        auth.signOut()
+        updateUI(null)
+    }
 
     private fun updateUI(user: FirebaseUser?) {
-       user?.let {
-           startActivity(Intent(this,MainActivity::class.java).putExtra(USER,user))
-       }
+        layout_logout.visibility = View.GONE
+        layout_login.visibility = View.VISIBLE
+        user?.let {
+            layout_logout.visibility = View.VISIBLE
+            layout_login.visibility = View.GONE
+            image_user.load(it.photoUrl)
+            text_email.text = it.displayName
+            text_phone.text = it.phoneNumber
+        }
     }
 }
